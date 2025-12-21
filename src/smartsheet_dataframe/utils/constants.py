021004e8239ab94140ac6a08281203eb4574038a
RCoff <coffman.ridge@gmail.com>
2025-12-21 15:38:46 -0500

    Add tests for constants

--- src/smartsheet_dataframe/utils/constants.py
+++ src/smartsheet_dataframe/utils/constants.py
@@ -4,9 +4,10 @@
 from typing import Final
 from urllib.parse import urljoin
 
-SMARTSHEET_API_BASE_URL: Final[str] = "https://api.smartsheet.com/2.0"
-SHEETS_ENDPOINT: Final[str] = urljoin(SMARTSHEET_API_BASE_URL, "sheets/")
-REPORTS_ENDPOINT: Final[str] = urljoin(SMARTSHEET_API_BASE_URL, "reports/")
+SMARTSHEET_API_BASE_URL: Final[str] = "https://api.smartsheet.com/"
+SMARTSHEET_API_V2_BASE_URL: Final[str] = urljoin(SMARTSHEET_API_BASE_URL, "2.0/")
+SHEETS_ENDPOINT: Final[str] = urljoin(SMARTSHEET_API_V2_BASE_URL, "sheets/")
+REPORTS_ENDPOINT: Final[str] = urljoin(SMARTSHEET_API_V2_BASE_URL, "reports/")
 
 REPORT: Final[str] = "REPORT"
 SHEET: Final[str] = "SHEET"
--- tests/test_utils/test_constants.py
+++ tests/test_utils/test_constants.py
@@ -1,6 +1,39 @@
 # Local Imports
 from smartsheet_dataframe.utils.constants import (
+    REPORT,
+    SHEET,
     REPORTS_ENDPOINT,
     SHEETS_ENDPOINT,
     SMARTSHEET_API_BASE_URL,
+    SMARTSHEET_API_V2_BASE_URL,
 )
+
+
+def test_report_constant():
+    """Test the REPORT constant."""
+    assert REPORT == "REPORT"
+
+
+def test_sheet_constant():
+    """Test the SHEET constant."""
+    assert SHEET == "SHEET"
+
+
+def test_smartsheet_api_base_url_constant():
+    """Test the SMARTSHEET_API_BASE_URL constant."""
+    assert SMARTSHEET_API_BASE_URL == "https://api.smartsheet.com/"
+
+
+def test_smartsheet_api_v2_base_url_constant():
+    """Test the SMARTSHEET_API_BASE_URL constant."""
+    assert SMARTSHEET_API_V2_BASE_URL == "https://api.smartsheet.com/2.0/"
+
+
+def test_sheets_endpoint_constant():
+    """Test the SHEETS_ENDPOINT constant."""
+    assert SHEETS_ENDPOINT == "https://api.smartsheet.com/2.0/sheets/"
+
+
+def test_reports_endpoint_constant():
+    """Test the REPORTS_ENDPOINT constant."""
+    assert REPORTS_ENDPOINT == "https://api.smartsheet.com/2.0/reports/"
